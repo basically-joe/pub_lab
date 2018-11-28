9b650c0e3d991049c6622e98ea12e9f7e3f783b8
Joe McColl <joe4kool@gmail.com>
2018-11-28 11:03:00 +0000

    drink class created and tests passing

---
+++ spec/drink_spec.rb
@@ -0,0 +1,16 @@
+require("minitest/rg")
+require("minitest/autorun")
+
+require_relative("../drink")
+
+class DrinkTest < MiniTest::Test
+
+  def setup
+    @drink = Drink.new("beer", "2.0", "5%")
+  end
+
+def test_can_create_drink
+  assert_equal(Drink, @drink.class())
+end
+
+end
